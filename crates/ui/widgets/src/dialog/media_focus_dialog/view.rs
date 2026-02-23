19750b4ee46989bad50f2096406cbc687d0e038e
nabbisen <nabbisen@scqr.net>
2026-02-23 12:54:45 +0900

    media focus image view improved

--- crates/ui/widgets/src/dialog/media_focus_dialog/view.rs
+++ crates/ui/widgets/src/dialog/media_focus_dialog/view.rs
@@ -1,21 +1,31 @@
-use iced::Element;
-use iced::Length::Fill;
-use iced::widget::image::Handle;
-use iced::widget::{button, column, container, image, scrollable};
+use iced::widget::{
+    image::Handle,
+    scrollable::{Direction, Scrollbar},
+};
+use iced::{
+    Element,
+    Length::Fill,
+    widget::{button, column, container, image, scrollable},
+};
 
 use super::{MediaFocusDialog, message::Message};
 
 impl MediaFocusDialog {
     pub fn view(&self) -> Element<'_, Message> {
         let handle = Handle::from_path(&self.path);
-        // todo: improve view
         let content = scrollable(
             container(image(handle))
                 .width(Fill)
                 .height(Fill)
                 .center(Fill)
-                .padding(40),
-        );
+                .padding(10),
+        )
+        .direction(Direction::Both {
+            vertical: Scrollbar::default(),
+            horizontal: Scrollbar::default(),
+        })
+        .width(Fill)
+        .height(Fill);
         let close_button = container(button("Close").on_press(Message::CloseClick)).center_x(Fill);
         column![content, close_button].into()
     }
