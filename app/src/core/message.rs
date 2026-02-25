b62dc8b0c75ba0941f2dd8eb8547a877aea9bdff
nabbisen <nabbisen@scqr.net>
2026-02-25 09:02:49 +0900

    context menu item to open with file manager

--- app/src/core/message.rs
+++ app/src/core/message.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use arama_ui_layout::{aside, footer, header};
 use arama_ui_main::views::gallery;
 use arama_ui_widgets::dialog::{media_focus_dialog, settings_dialog, similar_pairs_dialog};
@@ -12,6 +14,7 @@ pub enum Message {
     MediaFocusDialogMessage(media_focus_dialog::message::Message),
     SimilarPairsDialogMessage(similar_pairs_dialog::message::Message),
     SettingsDialogMessage(settings_dialog::message::Message),
+    FileManagerShow(PathBuf),
     DialogClose,
     CursorMove(Point),
 }
