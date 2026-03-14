bf1462bccd1f03b9daf376b4edb9b5809e2a7799
nabbisen <nabbisen@scqr.net>
2026-03-14 17:31:27 +0900

    video feature weight rethink: image : audio

--- crates/ai/src/lib.rs
+++ crates/ai/src/lib.rs
@@ -1,7 +1,7 @@
 // todo: parameters for inference quality - where to define actually ?
 const CLIP_IMAGE_SIZE: usize = 224;
 const CROSS_MAX_SIMILARITY_THRESHOLD: f32 = 0.25;
-const VIDEO_IMAGE_WEIGHT: f32 = 0.95;
+const VIDEO_IMAGE_WEIGHT: f32 = 0.60;
 macro_rules! video_audio_weight {
     () => {
         1.0 - VIDEO_IMAGE_WEIGHT
